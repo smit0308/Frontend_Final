3f43a810ec4c93bb8fcbc9adf716e881ab028ef6
Smit Patel <113251493+smit0308@users.noreply.github.com>
2025-05-14 12:13:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react({
-      include: ['**/*.jsx', '**/*.js'],  // tell Vite to treat .js files as JSX too
-      jsxRuntime: 'automatic',
-    })
-  ],
+  
+  plugins: [react()],
   base: './',
   build: {
     outDir: 'build',
